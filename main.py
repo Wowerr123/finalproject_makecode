90f59df6a0d69dc90c338641b2e27f49ac5d2d26
Wowerr123 <160294729+Wowerr123@users.noreply.github.com>
2024-04-16 23:30:51 +0100

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,45 +1,159 @@
+def soundofnoteselected(sprite: game.LedSprite):
+    if sprite.get(LedSpriteProperty.Y) == 0:
+        music.play(music.tone_playable(440, music.beat(BeatFraction.WHOLE)),
+            music.PlaybackMode.UNTIL_DONE)
+        storednotes[sprite.get(LedSpriteProperty.X)] = music.tone_playable(440, music.beat(BeatFraction.WHOLE))
+    elif sprite.get(LedSpriteProperty.Y) == 1:
+        music.play(music.tone_playable(349, music.beat(BeatFraction.WHOLE)),
+            music.PlaybackMode.UNTIL_DONE)
+        storednotes[sprite.get(LedSpriteProperty.X)] = music.tone_playable(349, music.beat(BeatFraction.WHOLE))
+    elif sprite.get(LedSpriteProperty.Y) == 2:
+        music.play(music.tone_playable(294, music.beat(BeatFraction.WHOLE)),
+            music.PlaybackMode.UNTIL_DONE)
+        storednotes[sprite.get(LedSpriteProperty.X)] = music.tone_playable(294, music.beat(BeatFraction.WHOLE))
+    elif sprite.get(LedSpriteProperty.Y) == 3:
+        music.play(music.tone_playable(247, music.beat(BeatFraction.WHOLE)),
+            music.PlaybackMode.UNTIL_DONE)
+        storednotes[sprite.get(LedSpriteProperty.X)] = music.tone_playable(247, music.beat(BeatFraction.WHOLE))
+    elif sprite.get(LedSpriteProperty.Y) == 4:
+        music.play(music.tone_playable(220, music.beat(BeatFraction.WHOLE)),
+            music.PlaybackMode.UNTIL_DONE)
+        storednotes[sprite.get(LedSpriteProperty.X)] = music.tone_playable(220, music.beat(BeatFraction.WHOLE))
+
 def on_button_pressed_a():
-    global scrolldata
+    global scrolldata, mousey
     if currentscreen == 1:
         # Change the number to how many options  you can scroll through.
         if scrolldata == 0:
             scrolldata = 1
         else:
             scrolldata += -1
-    else:
-        pass
+    if currentscreen == 2:
+        # Change the number to how many options  you can scroll through.
+        if mousey == 4:
+            mousey = 0
+            sprite2.set(LedSpriteProperty.Y, 0)
+        else:
+            mousey += 1
+            sprite2.change(LedSpriteProperty.Y, 1)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
-    global currentscreen
+    global currentscreen, spritec1, c1taken, spritec2, c2taken, spritec3, c3taken, spritec4, c4taken, spritec5, c5taken
     if currentscreen == 1:
         # Change the number to how many options  you can scroll through.
         if scrolldata == 1:
             currentscreen = 2
-        else:
-            pass
-    else:
-        pass
+    if currentscreen == 2 and screentwostuffallowed == 1:
+        if sprite2.get(LedSpriteProperty.X) == 0:
+            soundofnoteselected(sprite2)
+            if c1taken == 1:
+                spritec1.set(LedSpriteProperty.X, sprite2.get(LedSpriteProperty.X))
+                spritec1.set(LedSpriteProperty.Y, sprite2.get(LedSpriteProperty.Y))
+            else:
+                spritec1 = game.create_sprite(sprite2.get(LedSpriteProperty.X),
+                    sprite2.get(LedSpriteProperty.Y))
+                c1taken = 1
+        if sprite2.get(LedSpriteProperty.X) == 1:
+            soundofnoteselected(sprite2)
+            if c2taken == 1:
+                spritec2.set(LedSpriteProperty.X, sprite2.get(LedSpriteProperty.X))
+                spritec2.set(LedSpriteProperty.Y, sprite2.get(LedSpriteProperty.Y))
+            else:
+                spritec2 = game.create_sprite(sprite2.get(LedSpriteProperty.X),
+                    sprite2.get(LedSpriteProperty.Y))
+                c2taken = 1
+        if sprite2.get(LedSpriteProperty.X) == 2:
+            soundofnoteselected(sprite2)
+            if c3taken == 1:
+                spritec3.set(LedSpriteProperty.X, sprite2.get(LedSpriteProperty.X))
+                spritec3.set(LedSpriteProperty.Y, sprite2.get(LedSpriteProperty.Y))
+            else:
+                spritec3 = game.create_sprite(sprite2.get(LedSpriteProperty.X),
+                    sprite2.get(LedSpriteProperty.Y))
+                c3taken = 1
+        if sprite2.get(LedSpriteProperty.X) == 3:
+            soundofnoteselected(sprite2)
+            if c4taken == 1:
+                spritec4.set(LedSpriteProperty.X, sprite2.get(LedSpriteProperty.X))
+                spritec4.set(LedSpriteProperty.Y, sprite2.get(LedSpriteProperty.Y))
+            else:
+                spritec4 = game.create_sprite(sprite2.get(LedSpriteProperty.X),
+                    sprite2.get(LedSpriteProperty.Y))
+                c4taken = 1
+        if sprite2.get(LedSpriteProperty.X) == 4:
+            soundofnoteselected(sprite2)
+            if c5taken == 1:
+                spritec5.set(LedSpriteProperty.X, sprite2.get(LedSpriteProperty.X))
+                spritec5.set(LedSpriteProperty.Y, sprite2.get(LedSpriteProperty.Y))
+            else:
+                spritec5 = game.create_sprite(sprite2.get(LedSpriteProperty.X),
+                    sprite2.get(LedSpriteProperty.Y))
+                c5taken = 1
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
-    global scrolldata
+    global scrolldata, mousex
     if currentscreen == 1:
         # Change the number to how many options  you can scroll through.
         if scrolldata == 3:
             scrolldata = 1
         else:
             scrolldata += 1
-    else:
-        pass
+    if currentscreen == 2:
+        # Change the number to how many options  you can scroll through.
+        if mousex == 4:
+            mousex = 0
+            sprite2.set(LedSpriteProperty.X, 0)
+        else:
+            mousex += 1
+            sprite2.change(LedSpriteProperty.X, 1)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+def on_gesture_shake():
+    for value in storednotes:
+        music.play(value, music.PlaybackMode.UNTIL_DONE)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+pressuresensorvalue = 0
+mousex = 0
+spritec5: game.LedSprite = None
+spritec4: game.LedSprite = None
+spritec3: game.LedSprite = None
+spritec2: game.LedSprite = None
+spritec1: game.LedSprite = None
+sprite2: game.LedSprite = None
+mousey = 0
+storednotes: List[music.Playable] = []
+screentwostuffallowed = 0
+c5taken = 0
+c4taken = 0
+c3taken = 0
+c2taken = 0
+c1taken = 0
 scrolldata = 0
 currentscreen = 0
 currentscreen = 1
 scrolldata = 1
+spriteforeverscreenstop = 1
+c1taken = 0
+c2taken = 0
+c3taken = 0
+c4taken = 0
+c5taken = 0
+screentwostuffallowed = 0
+storednotes = []
 
 def on_forever():
+    with open('storedsongs.txt', 'w') as file:
+        file.write("test")  # Write an empty string to create the file if it doesn't exist
+    basic.pause(1000)  # Pause for 1 second to ensure file creation
+    # Add other code here
+basic.forever(on_forever)
+
+
+def on_forever2():
+    global sprite2, spriteforeverscreenstop, screentwostuffallowed, pressuresensorvalue
     if currentscreen == 1:
         # Change the number to how many options  you can scroll through.
         if scrolldata == 1:
@@ -53,50 +167,11 @@ def on_forever():
                 . # # . .
                 . # . . .
                 """)
+    if currentscreen == 2 and spriteforeverscreenstop == 1:
+        sprite2 = game.create_sprite(0, 0)
+        spriteforeverscreenstop = 0
     if currentscreen == 2:
-        # Change the number to how many options  you can scroll through.
-        if scrolldata == 1:
-            basic.show_leds("""
-                # . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                """)
-        # Change the number to how many options  you can scroll through.
-        if scrolldata == 2:
-            basic.show_leds("""
-                . # . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                """)
-        # Change the number to how many options  you can scroll through.
-        if scrolldata == 3:
-            basic.show_leds("""
-                . . # . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                """)
-        # Change the number to how many options  you can scroll through.
-        if scrolldata == 4:
-            basic.show_leds("""
-                . . . # .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                """)
-        # Change the number to how many options  you can scroll through.
-        if scrolldata == 5:
-            basic.show_leds("""
-                . . . . #
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                """)
-basic.forever(on_forever)
+        sprite2.set(LedSpriteProperty.BLINK, 400)
+        screentwostuffallowed = 1
+    pressuresensorvalue = pins.digital_read_pin(DigitalPin.P1)
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+function soundofnoteselected (sprite: game.LedSprite) {
+    if (sprite.get(LedSpriteProperty.Y) == 0) {
+        music.play(music.tonePlayable(440, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        storednotes[sprite.get(LedSpriteProperty.X)] = music.tonePlayable(440, music.beat(BeatFraction.Whole))
+    } else if (sprite.get(LedSpriteProperty.Y) == 1) {
+        music.play(music.tonePlayable(349, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        storednotes[sprite.get(LedSpriteProperty.X)] = music.tonePlayable(349, music.beat(BeatFraction.Whole))
+    } else if (sprite.get(LedSpriteProperty.Y) == 2) {
+        music.play(music.tonePlayable(294, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        storednotes[sprite.get(LedSpriteProperty.X)] = music.tonePlayable(294, music.beat(BeatFraction.Whole))
+    } else if (sprite.get(LedSpriteProperty.Y) == 3) {
+        music.play(music.tonePlayable(247, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        storednotes[sprite.get(LedSpriteProperty.X)] = music.tonePlayable(247, music.beat(BeatFraction.Whole))
+    } else if (sprite.get(LedSpriteProperty.Y) == 4) {
+        music.play(music.tonePlayable(220, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        storednotes[sprite.get(LedSpriteProperty.X)] = music.tonePlayable(220, music.beat(BeatFraction.Whole))
+    }
+}
 input.onButtonPressed(Button.A, function () {
     if (currentscreen == 1) {
         // Change the number to how many options  you can scroll through.
@@ -11,10 +29,10 @@ input.onButtonPressed(Button.A, function () {
         // Change the number to how many options  you can scroll through.
         if (mousey == 4) {
             mousey = 0
-            sprite.set(LedSpriteProperty.Y, 0)
+            sprite2.set(LedSpriteProperty.Y, 0)
         } else {
             mousey += 1
-            sprite.change(LedSpriteProperty.Y, 1)
+            sprite2.change(LedSpriteProperty.Y, 1)
         }
     }
 })
@@ -25,50 +43,54 @@ input.onButtonPressed(Button.AB, function () {
             currentscreen = 2
         }
     }
-    if (currentscreen == 2) {
-        basic.pause(500)
-        if (sprite.get(LedSpriteProperty.X) == 1) {
+    if (currentscreen == 2 && screentwostuffallowed == 1) {
+        if (sprite2.get(LedSpriteProperty.X) == 0) {
+            soundofnoteselected(sprite2)
             if (c1taken == 1) {
-                spritec1.set(LedSpriteProperty.X, sprite.get(LedSpriteProperty.X))
-                spritec1.set(LedSpriteProperty.Y, sprite.get(LedSpriteProperty.Y))
+                spritec1.set(LedSpriteProperty.X, sprite2.get(LedSpriteProperty.X))
+                spritec1.set(LedSpriteProperty.Y, sprite2.get(LedSpriteProperty.Y))
             } else {
-                spritec1 = game.createSprite(sprite.get(LedSpriteProperty.X), sprite.get(LedSpriteProperty.Y))
+                spritec1 = game.createSprite(sprite2.get(LedSpriteProperty.X), sprite2.get(LedSpriteProperty.Y))
                 c1taken = 1
             }
         }
-        if (sprite.get(LedSpriteProperty.X) == 2) {
+        if (sprite2.get(LedSpriteProperty.X) == 1) {
+            soundofnoteselected(sprite2)
             if (c2taken == 1) {
-                spritec2.set(LedSpriteProperty.X, sprite.get(LedSpriteProperty.X))
-                spritec2.set(LedSpriteProperty.Y, sprite.get(LedSpriteProperty.Y))
+                spritec2.set(LedSpriteProperty.X, sprite2.get(LedSpriteProperty.X))
+                spritec2.set(LedSpriteProperty.Y, sprite2.get(LedSpriteProperty.Y))
             } else {
-                spritec2 = game.createSprite(sprite.get(LedSpriteProperty.X), sprite.get(LedSpriteProperty.Y))
+                spritec2 = game.createSprite(sprite2.get(LedSpriteProperty.X), sprite2.get(LedSpriteProperty.Y))
                 c2taken = 1
             }
         }
-        if (sprite.get(LedSpriteProperty.X) == 3) {
+        if (sprite2.get(LedSpriteProperty.X) == 2) {
+            soundofnoteselected(sprite2)
             if (c3taken == 1) {
-                spritec3.set(LedSpriteProperty.X, sprite.get(LedSpriteProperty.X))
-                spritec3.set(LedSpriteProperty.Y, sprite.get(LedSpriteProperty.Y))
+                spritec3.set(LedSpriteProperty.X, sprite2.get(LedSpriteProperty.X))
+                spritec3.set(LedSpriteProperty.Y, sprite2.get(LedSpriteProperty.Y))
             } else {
-                spritec3 = game.createSprite(sprite.get(LedSpriteProperty.X), sprite.get(LedSpriteProperty.Y))
+                spritec3 = game.createSprite(sprite2.get(LedSpriteProperty.X), sprite2.get(LedSpriteProperty.Y))
                 c3taken = 1
             }
         }
-        if (sprite.get(LedSpriteProperty.X) == 4) {
+        if (sprite2.get(LedSpriteProperty.X) == 3) {
+            soundofnoteselected(sprite2)
             if (c4taken == 1) {
-                spritec4.set(LedSpriteProperty.X, sprite.get(LedSpriteProperty.X))
-                spritec4.set(LedSpriteProperty.Y, sprite.get(LedSpriteProperty.Y))
+                spritec4.set(LedSpriteProperty.X, sprite2.get(LedSpriteProperty.X))
+                spritec4.set(LedSpriteProperty.Y, sprite2.get(LedSpriteProperty.Y))
             } else {
-                spritec4 = game.createSprite(sprite.get(LedSpriteProperty.X), sprite.get(LedSpriteProperty.Y))
+                spritec4 = game.createSprite(sprite2.get(LedSpriteProperty.X), sprite2.get(LedSpriteProperty.Y))
                 c4taken = 1
             }
         }
-        if (sprite.get(LedSpriteProperty.X) == 5) {
+        if (sprite2.get(LedSpriteProperty.X) == 4) {
+            soundofnoteselected(sprite2)
             if (c5taken == 1) {
-                spritec5.set(LedSpriteProperty.X, sprite.get(LedSpriteProperty.X))
-                spritec5.set(LedSpriteProperty.Y, sprite.get(LedSpriteProperty.Y))
+                spritec5.set(LedSpriteProperty.X, sprite2.get(LedSpriteProperty.X))
+                spritec5.set(LedSpriteProperty.Y, sprite2.get(LedSpriteProperty.Y))
             } else {
-                spritec5 = game.createSprite(sprite.get(LedSpriteProperty.X), sprite.get(LedSpriteProperty.Y))
+                spritec5 = game.createSprite(sprite2.get(LedSpriteProperty.X), sprite2.get(LedSpriteProperty.Y))
                 c5taken = 1
             }
         }
@@ -87,31 +109,53 @@ input.onButtonPressed(Button.B, function () {
         // Change the number to how many options  you can scroll through.
         if (mousex == 4) {
             mousex = 0
-            sprite.set(LedSpriteProperty.X, 0)
+            sprite2.set(LedSpriteProperty.X, 0)
         } else {
             mousex += 1
-            sprite.change(LedSpriteProperty.X, 1)
+            sprite2.change(LedSpriteProperty.X, 1)
         }
     }
 })
+input.onGesture(Gesture.Shake, function () {
+    for (let value of storednotes) {
+        music.play(value, music.PlaybackMode.UntilDone)
+    }
+})
+let pressuresensorvalue = 0
 let mousex = 0
 let spritec5: game.LedSprite = null
-let c5taken = 0
 let spritec4: game.LedSprite = null
-let c4taken = 0
 let spritec3: game.LedSprite = null
-let c3taken = 0
 let spritec2: game.LedSprite = null
-let c2taken = 0
 let spritec1: game.LedSprite = null
-let c1taken = 0
-let sprite: game.LedSprite = null
+let sprite2: game.LedSprite = null
 let mousey = 0
+let storednotes: music.Playable[] = []
+let screentwostuffallowed = 0
+let c5taken = 0
+let c4taken = 0
+let c3taken = 0
+let c2taken = 0
+let c1taken = 0
 let scrolldata = 0
 let currentscreen = 0
 currentscreen = 1
 scrolldata = 1
 let spriteforeverscreenstop = 1
+c1taken = 0
+c2taken = 0
+c3taken = 0
+c4taken = 0
+c5taken = 0
+screentwostuffallowed = 0
+storednotes = []
+basic.forever(function () {
+    if (pressuresensorvalue == 1 && currentscreen == 2) {
+    	
+    } else {
+    	
+    }
+})
 basic.forever(function () {
     if (currentscreen == 1) {
         // Change the number to how many options  you can scroll through.
@@ -130,10 +174,12 @@ basic.forever(function () {
         }
     }
     if (currentscreen == 2 && spriteforeverscreenstop == 1) {
-        sprite = game.createSprite(3, 2)
+        sprite2 = game.createSprite(0, 0)
         spriteforeverscreenstop = 0
     }
     if (currentscreen == 2) {
-        sprite.set(LedSpriteProperty.Blink, 400)
+        sprite2.set(LedSpriteProperty.Blink, 400)
+        screentwostuffallowed = 1
     }
+    pressuresensorvalue = pins.digitalReadPin(DigitalPin.P1)
 })
